e045046b8f7ab9448fa0db848bc821f7fc75f021
Xime <ximecamino@gmail.com>
2025-05-22 10:22:32 -0300

    link del airdrop

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,7 +99,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://inflora-play.vercel.app/',
+            href: 'https://airdrop.inflora.cloud/',
             label: 'FOUNDATIONAL AIRDROP',
             position: 'right',
           },
